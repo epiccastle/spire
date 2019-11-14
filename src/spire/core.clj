1153a7279491f90a6b3889d60c388394272e8863
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-15 00:37:45 +0800

    lsb_release -a

--- src/spire/core.clj
+++ src/spire/core.clj
@@ -4,14 +4,28 @@
             [clojure.string :as string])
   (:gen-class))
 
+(defn to-camelcase [s]
+  (-> s
+      (string/split #"\s+")
+      (->> (map string/lower-case)
+           (string/join "-"))))
+
+(defn lsb-process [out]
+  (-> out
+      (string/split #"\n")
+      (->> (map #(string/split % #":\s+"))
+           (map (fn [[k v]] [(-> k to-camelcase keyword) v]))
+           (into {}))))
+
 (defn -main
   "ssh to ourselves and collect paths"
   [& args]
   (let [proc (sh/proc ["ssh" (or (first args) "localhost")])
-        which (fn [command]
-                (let [{:keys [out exit]} (sh/run proc (str "which " command))]
+        run (fn [command]
+                (let [{:keys [out exit]} (sh/run proc command)]
                   (when (zero? exit)
                     (string/trim out))))
+        which #(run (str "which " %))
         paths
         {:md5sum (which "md5sum")
          :crc32 (which "crc32")
@@ -22,5 +36,13 @@
          :sha384sum (which "sha384sum")
          :curl (which "curl")
          :wget (which "wget")
-         }]
-    (puget/cprint paths)))
+         :apt (which "apt")
+         :apt-get (which "apt-get")
+         :apt-cache (which "apt-cache")
+         :rpm (which "rpm")
+         :yum (which "yum")
+         }
+
+        lsb-release (some-> "lsb_release -a" run lsb-process)]
+    (puget/cprint {:paths paths
+                   :lsb-release lsb-release})))
