2577fd094cbbfb899deac4914516870030f057e5
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-15 23:31:30 +0800

    more deps

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [fipp "0.6.21"]
-                 [mvxcvi/puget "1.2.0"]]
+                 [mvxcvi/puget "1.2.0"]
+                 [digest "1.4.9"]
+                 [clj-time "0.15.2"]]
   :main ^:skip-aot spire.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
