2ab67db1fe1c9e81c94571f81c9a39ea6638430f
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-16 17:01:43 +0800

    break version out into a text file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spire "0.1.0-SNAPSHOT"
+(defproject spire #= (clojure.string/trim #= (slurp ".meta/VERSION"))
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
