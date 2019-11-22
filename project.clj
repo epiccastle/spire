9ca725648e76cb60dcd4c7a844203c275752308f
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-22 23:51:16 +0800

    commons codec for hashing, encoding and decoding

--- project.clj
+++ project.clj
@@ -15,6 +15,9 @@
                  [nrepl "0.6.0"]
 
                  [com.jcraft/jsch "0.1.55"]
+
+                 ;; base64
+                 [commons-codec/commons-codec "1.12"]
                  ]
   :plugins [[cider/cider-nrepl "0.21.1"]]
   :source-paths ["src/clj"]
