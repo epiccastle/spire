d2284b49d4490d5370beb38e8d84c0b883ad8c75
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-24 12:03:11 +0800

    passphrase decrypt prompting

--- src/clj/spire/ssh.clj
+++ src/clj/spire/ssh.clj
@@ -1,32 +1,6 @@
 (ns spire.ssh
   (:import [com.jcraft.jsch UserInfo]))
 
-;; #define SANE_SET 1		/* Set in `sane' mode. */
-;; #define SANE_UNSET 2		/* Unset in `sane' mode. */
-;; #define REV 4			/* Can be turned off by prepending `-'. */
-;; #define OMIT 8			/* Don't display value. */
-
-;; termios.h
-;; #define ECHO	0000010
-;; #define ECHOE	0000020
-;; #define ECHOK	0000040
-;; #define ECHONL	0000100
-
-;; name             /* Name given on command line.  */
-;; mode_type type   /* Which structure element to change. */
-;; flags            /* Setting and display options.  */
-;; bits             /* Bits to set for this mode.  */
-;; mask             /* Other bits to turn off for this mode.  */
-
-;; "echo", local, SANE_SET | REV, ECHO, 0
-
-;; display_settings (output_type, &mode, device_name);
-
-;; match_found = set_mode (&mode_info[i], reversed, &mode);
-;; require_set_attr = true;
-
-
-
 (defn make-user-info []
   (let [state (atom nil)]
     (proxy [UserInfo] []
@@ -48,12 +22,20 @@
           (boolean (#{\y \Y} first-char))))
 
       (getPassphrase []
-        (println "getPassphrase")
-        "foo")
+        (print (str "Enter " @state ": "))
+        (.flush *out*)
+        (SpireUtils/enter-raw-mode 0)
+        (let [password (read-line)]
+          (SpireUtils/leave-raw-mode 0)
+          (println)
+          password))
 
       (promptPassphrase [s]
-        (println 2 s)
-        true)
+        (reset! state s)
+        ;; true decrypt key
+        ;; false cancel key decrypt
+        true
+        )
 
       (promptPassword [s]
         (reset! state s)
@@ -63,13 +45,4 @@
         )
 
       (showMessage [s]
-        (println 4 s))
-
-      (promptKeyboardInteractive [dest name inst prompt echo]
-        (println "promptKeyboardInteractive")
-        (println dest)
-        (println name)
-        (println inst)
-        (println prompt)
-        (println echo)
-        "yes"))))
+        (println s)))))
