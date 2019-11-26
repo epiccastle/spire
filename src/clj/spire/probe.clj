5807acea9dedd35d624799aefad4b1b3156a519d
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-27 00:56:58 +0800

    get arch facts as well as path locations

--- src/clj/spire/probe.clj
+++ src/clj/spire/probe.clj
@@ -8,16 +8,24 @@
       (when (zero? exit)
         (string/trim out)))))
 
-(defn commands [runner]
+(defn facts [runner]
   (let [run (make-run runner)
         which #(run (str "which " %))
-        paths {:md5sum (which "md5sum")
-               :crc32 (which "crc32")
-               :sha256sum (which "sha256sum")
-               :curl (which "curl")
-               :wget (which "wget")
-               :ping (which "ping")}]
-    paths))
+        uname #(run (str "uname " %))]
+    {:paths {:md5sum (which "md5sum")
+             :crc32 (which "crc32")
+             :sha256sum (which "sha256sum")
+             :curl (which "curl")
+             :wget (which "wget")
+             :ping (which "ping")}
+     :arch {:kernel {:name (uname "-s")
+                     :release (uname "-r")
+                     :version (uname "-v")}
+            :network-node (uname "-n")
+            :machine (uname "-m")
+            :processor (uname "-p")
+            :platform (uname "-i")
+            :os (uname "-o")}}))
 
 (defn lsb-release [runner]
   (let [run (make-run runner)]
