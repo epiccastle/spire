9c9ce20c433ced05723722508499b61ef4845406
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-29 00:36:29 +0800

    break out printing of form

--- src/clj/spire/system.clj
+++ src/clj/spire/system.clj
@@ -10,53 +10,51 @@
 (defn apt-get [& args]
   (transport/sh (string/join " " (concat [apt-command] args)) "" ""))
 
-(defmulti apt (fn [state & args] state))
+(defmulti apt* (fn [state & args] state))
 
-(defmethod apt :update [_]
-  (print "(apt :update)")
-  (.flush *out*)
+(defmethod apt* :update [_]
   (apt-get "update"))
 
-(defmethod apt :upgrade [_]
-  (print "(apt :upgrade)")
-  (.flush *out*)
+(defmethod apt* :upgrade [_]
   (apt-get "upgrade" "-y"))
 
-(defmethod apt :dist-upgrade [_]
+(defmethod apt* :dist-upgrade [_]
   (apt-get "dist-upgrade" "-y"))
 
-(defmethod apt :autoremove [_]
+(defmethod apt* :autoremove [_]
   (apt-get "autoremove" "-y"))
 
-(defmethod apt :clean [_]
+(defmethod apt* :clean [_]
   (apt-get "clean" "-y"))
 
-(defmethod apt :install [_ package-or-packages]
-  (print (format "(apt :install %s)" (pr-str package-or-packages)))
-  (.flush *out*)
+(defmethod apt* :install [_ package-or-packages]
   (if (string? package-or-packages)
     (apt-get "install" "-y" package-or-packages)
     (apt-get "install" "-y" (string/join " " package-or-packages))))
 
-(defmethod apt :remove [_ package-or-packages]
-  (print (format "(apt :remove %s)" (pr-str package-or-packages)))
-  (.flush *out*)
+(defmethod apt* :remove [_ package-or-packages]
   (if (string? package-or-packages)
     (apt-get "remove" "-y" package-or-packages)
     (apt-get "remove" "-y" (string/join " " package-or-packages))))
 
-(defmethod apt :purge [_ package-or-packages]
+(defmethod apt* :purge [_ package-or-packages]
   (if (string? package-or-packages)
     (apt-get "purge" "-y" package-or-packages)
     (apt-get "purge" "-y" (string/join " " package-or-packages))))
 
-(defmethod apt :download [_ package-or-packages]
+(defmethod apt* :download [_ package-or-packages]
   (if (string? package-or-packages)
     (apt-get "download" "-y" package-or-packages)
     (apt-get "download" "-y" (string/join " " package-or-packages))))
 
-#_ (apt :download ["iputils-ping" "traceroute"])
-#_ (apt :autoremove)
+(defn apt [& args]
+  (pr (concat '(apt) args))
+  (.flush *out*)
+  (apply apt* args))
+
+
+#_ (apt* :download ["iputils-ping" "traceroute"])
+#_ (apt* :autoremove)
 
 (defn hostname [hostname]
   (spit "/etc/hostname" hostname)
