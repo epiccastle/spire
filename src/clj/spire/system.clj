7b55075dfbe3dcb8cbd8e44886a54d1bc5ca7c84
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-29 01:04:33 +0800

    start processing apt results

--- src/clj/spire/system.clj
+++ src/clj/spire/system.clj
@@ -10,10 +10,33 @@
 (defn apt-get [& args]
   (transport/sh (string/join " " (concat [apt-command] args)) "" ""))
 
+(defn process-values [result func]
+  (->> result
+       (map (fn [[k v]] [k (func v)]))
+       (into {})))
+
+(defn process-apt-update-line [line]
+  (let [[method remain] (string/split line #":" 2)]
+    [method remain]
+    )
+  )
+
 (defmulti apt* (fn [state & args] state))
 
 (defmethod apt* :update [_]
-  (apt-get "update"))
+  (-> (apt-get "update")
+      (process-values
+       (fn [{:keys [exit out] :as result}]
+         (if (zero? exit)
+           (assoc result
+                  :result :ok
+                  :out-lines (string/split out #"\n")
+                  :data (-> out
+                            (string/split #"\n")
+                            (->> (map process-apt-update-line))
+                            )
+                  )
+           (assoc result :result :fail))))))
 
 (defmethod apt* :upgrade [_]
   (apt-get "upgrade" "-y"))
