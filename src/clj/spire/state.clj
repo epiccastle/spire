9bd7195f5f65eb8fd1c1f8555ca5e2b86d64ed81
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-30 22:00:35 +0800

    *connections* thread local

--- src/clj/spire/state.clj
+++ src/clj/spire/state.clj
@@ -1,4 +1,12 @@
 (ns spire.state)
 
-; the currect execution sessions
+;; the currect execution sessions that each idempotent call will use
+;; in lockstep mode, this will be all the current connection
+;; in parallel mode this will usually be one of the current connections
+;; is a vector of host strings
 (def ^:dynamic *sessions* [])
+
+;; all the host strings needed to complete a step or action
+;; this will be the complete set of hosts to operate on and will be
+;; the same in parallel or lockstep modes
+(def ^:dynamic *connections* [])
