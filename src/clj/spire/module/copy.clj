9b6db2c9e94d7243920aea73f9e4fd451164d1d2
Crispin Wellington <retrogradeorbit@gmail.com>
2019-12-08 00:25:41 +0800

    add attr setting to copy module

--- src/clj/spire/module/copy.clj
+++ src/clj/spire/module/copy.clj
@@ -3,11 +3,12 @@
             [spire.ssh :as ssh]
             [spire.scp :as scp]
             [spire.utils :as utils]
+            [spire.module.attrs :as attrs]
             [digest :as digest]
             [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(utils/defmodule copy [{:keys [src dest]}]
+(utils/defmodule copy [{:keys [src dest owner group mode attrs]}]
   [host-string session]
   (let [run (fn [command]
               (let [{:keys [out exit]}
@@ -18,8 +19,32 @@
         remote-md5 (some-> (run (format "%s -b \"%s\"" "md5sum" dest))
                            (string/split #"\s+")
                            first)]
-    (if (= local-md5 remote-md5)
-      {:result :ok}
-      (do
-        (scp/scp-to session src dest :progress-fn (fn [& args] (output/print-progress host-string args)))
-        {:result :changed}))))
+    (let [copied?
+          (if (= local-md5 remote-md5)
+            false
+            (do
+              (scp/scp-to session src dest :progress-fn (fn [& args] (output/print-progress host-string args)))
+              true))
+          passed-attrs? (or owner group mode attrs)]
+      (if (not passed-attrs?)
+        ;; just copied
+        {:result (if copied? :changed :ok)}
+
+        ;; run attrs
+        (let [{:keys [exit err out] :as result}
+              (attrs/set-attrs
+               session
+               {:path dest
+                :owner owner
+                :group group
+                :mode mode
+                :attrs attrs})]
+          (cond
+            (zero? exit)
+            {:result (if copied? :changed :ok)}
+
+            (= 255 exit)
+            (assoc result :result :changed)
+
+            :else
+            (assoc result :result :failed)))))))
