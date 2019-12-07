dddb0936c7537ad407e3df0b94a04cd75c06c467
Crispin Wellington <retrogradeorbit@gmail.com>
2019-12-07 18:49:37 +0800

    md5 before copy

--- src/clj/spire/module/copy.clj
+++ src/clj/spire/module/copy.clj
@@ -1,17 +1,25 @@
 (ns spire.module.copy
   (:require [spire.output :as output]
-            [spire.state :as state]
-            [spire.transport :as transport]
+            [spire.ssh :as ssh]
             [spire.scp :as scp]
             [spire.utils :as utils]
-            [clojure.string :as string])
-  )
+            [digest :as digest]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
 
 (utils/defmodule copy [{:keys [src dest]}]
   [host-string session]
-  (scp/scp-to session src dest
-              :progress-fn
-              ;;(fn [& args] (apply println host-string args))
-              ;;utils/progress-bar
-              (fn [& args] (output/print-progress host-string args))
-              ))
+  (let [run (fn [command]
+              (let [{:keys [out exit]}
+                    (ssh/ssh-exec session command "" "UTF-8" {})]
+                (when (zero? exit)
+                  (string/trim out))))
+        local-md5 (digest/md5 (io/as-file src))
+        remote-md5 (some-> (run (format "%s -b \"%s\"" "md5sum" dest))
+                           (string/split #"\s+")
+                           first)]
+    (if (= local-md5 remote-md5)
+      {:result :ok}
+      (do
+        (scp/scp-to session src dest :progress-fn (fn [& args] (output/print-progress host-string args)))
+        {:result :changed}))))
