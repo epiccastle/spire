8b5d3c93266bb051af8c0fea82ebf6fa6c937e89
Crispin Wellington <retrogradeorbit@gmail.com>
2019-12-15 00:24:53 +0800

    download test

---
+++ test/spire/module/download_test.clj
@@ -0,0 +1,26 @@
+(ns spire.module.download-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as string]
+            [clojure.java.shell :as shell]
+            [spire.module.download :refer :all]
+            [spire.module.attrs :refer :all]
+            [spire.transport :as transport]
+            [spire.ssh :as ssh]
+            [spire.state :as state]
+            [spire.config :as config]
+            [clojure.java.io :as io]
+            [spire.test-utils :as test-utils]))
+
+(clojure.lang.RT/loadLibrary "spire")
+
+(defn no-scp [& args]
+  (assert false "second copy should be skipped"))
+
+(deftest download-test
+  (testing "download test"
+    (transport/ssh
+     "localhost"
+     ;; copy
+     (is (= nil
+            (download {:src ".xmonad" :dest "/tmp/bashrc" :recurse true})))
+     )))
