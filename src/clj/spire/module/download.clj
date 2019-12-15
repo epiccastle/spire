d7c3417ea589798468a5cf73b2fcce33d68736af
Crispin Wellington <retrogradeorbit@gmail.com>
2019-12-16 00:07:14 +0800

    download changes

--- src/clj/spire/module/download.clj
+++ src/clj/spire/module/download.clj
@@ -4,7 +4,7 @@
             [spire.scp :as scp]
             [spire.utils :as utils]
             [spire.module.attrs :as attrs]
-            [spire.module.upload :as upload]
+            [spire.compare :as compare]
             [digest :as digest]
             [clojure.java.io :as io]
             [clojure.string :as string]))
@@ -12,7 +12,7 @@
 (defn preflight [opts]
   nil)
 
-(utils/defmodule download [{:keys [src dest recurse preserve flat] :as opts}]
+(utils/defmodule download [{:keys [src dest recurse preserve flat dir-mode mode] :as opts}]
   [host-string session]
   (or
    (preflight opts)
@@ -22,8 +22,34 @@
                  (when (zero? exit)
                    (string/trim out))))
 
-         {:keys [remote-to-local] :as comparison}
-         (upload/compare-local-and-remote dest run src)
+         {:keys [remote-to-local local-file?] :as comparison}
+         (compare/compare-local-and-remote dest run src)
          ]
-     (scp/scp-from session src dest :recurse recurse :preserve true
-                   ))))
+     (if recurse
+       (cond
+         (and local-file? (not force))
+         {:result :failed :err "Cannot copy remote `src` over local `dest`: destination is a file. Use :force to delete destination file and replace."}
+
+         (and local-file? force)
+         (do
+           (.delete (io/file dest))
+           (scp/scp-from session src dest
+                         :progress-fn (fn [file bytes total frac context]
+                                        #_ (output/print-progress
+                                         host-string
+                                         (utils/progress-stats
+                                          file bytes total frac
+                                          local-to-remote-total-size
+                                          local-to-remote-max-filename-length
+                                          context)
+                                         ))
+                         :preserve preserve
+                         :dir-mode (or dir-mode 0755)
+                         :mode (or mode 0644)
+                         :recurse true
+                         :skip-files #{}))
+
+
+         ))
+     #_ (scp/scp-from session src dest :recurse recurse :preserve true
+                      ))))
