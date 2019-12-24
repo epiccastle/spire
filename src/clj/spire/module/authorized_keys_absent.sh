817847c61b480e0e3e3c5a9af0ad133f4bc97090
Crispin Wellington <retrogradeorbit@gmail.com>
2019-12-25 00:25:34 +0800

    authorized_keys :absent shell script

---
+++ src/clj/spire/module/authorized_keys_absent.sh
@@ -0,0 +1,48 @@
+set -e
+
+if [ "$USER" ]; then
+  id -u "$USER" >& /dev/null
+  if [ $? -ne 0 ]; then
+    echo -n "User not found." 1>&2
+    exit 1
+  fi
+
+  HOME_DIR=$(bash -c "echo ~${USER}")
+  AUTHORIZED_KEYS_DIR="${HOME_DIR}/.ssh"
+  AUTHORIZED_KEYS_FILE="${HOME_DIR}/.ssh/authorized_keys"
+  GROUP=$(id -gn "${USER}")
+
+  if [ ! -d "$AUTHORIZED_KEYS_DIR" ]; then
+    mkdir -p "$AUTHORIZED_KEYS_DIR"
+    chown "$USER:$GROUP" "$AUTHORIZED_KEYS_DIR"
+    chmod 700 "$AUTHORIZED_KEYS_DIR"
+  fi
+
+  if [ ! -f "$AUTHORIZED_KEYS_FILE" ]; then
+    touch "$AUTHORIZED_KEYS_FILE"
+    chown "$USER:$GROUP" "$AUTHORIZED_KEYS_FILE"
+    chmod 664 "$AUTHORIZED_KEYS_FILE"
+  fi
+fi
+
+KEY_START=$(echo "$KEY" | cut -d " " -f 1-2)
+KEY_FOUND=$(grep -F -n "$KEY_START" "$AUTHORIZED_KEYS_FILE" || true)
+
+# if [ "$OPTIONS" ]; then
+#   KEY_LINE="$OPTIONS $KEY"
+# else
+#   KEY_LINE="$KEY"
+# fi
+
+if [ "$KEY_FOUND" ]; then
+  # key already in file
+  LINENUM=$(echo "$KEY_FOUND" | cut -d ":" -f 1)
+  sed -i "${LINENUM}d" "$AUTHORIZED_KEYS_FILE"
+  EXIT=-1
+else
+  # not found
+  EXIT=0
+fi
+
+cat "$AUTHORIZED_KEYS_FILE"
+exit $EXIT
