de8e95c778493238852eabd81cfb27d144f91535
Crispin Wellington <retrogradeorbit@gmail.com>
2019-12-27 15:47:07 +0800

    read shadow file parameters

--- src/clj/spire/module/user_present.sh
+++ src/clj/spire/module/user_present.sh
@@ -16,4 +16,22 @@ if [ "$PWD_LINE" ]; then
   echo 3 $OCOMMENT $COMMENT
   echo 4 $OHOME $HOME
   echo 5 $OSHELL $SHELL
+
+  IFS=':' read -r -a SHADOW <<< $(grep "^${NAME}:" /etc/shadow)
+
+  OPASSWORD="${SHADOW[1]}"
+  OLAST_CHANGE="${SHADOW[2]}"
+  OMIN_AGE="${SHADOW[3]}"
+  OMAX_AGE="${SHADOW[4]}"
+  OWARNING="${SHADOW[5]}"
+  OINACTIVE="${SHADOW[6]}"
+  OEXPIRE="${SHADOW[7]}"
+
+  echo "pass: $OPASSWORD"
+  echo "lastchange: $OLAST_CHANGE"
+  echo "minage: $OMIN_AGE"
+  echo "maxage: $OMAX_AGE"
+  echo "warning: $OWARNING"
+  echo "inactive: $OINACTIVE"
+  echo "exp: $OEXPIRE"
 fi
