e69e91df26466bd8a1414b941269194bfce031e9
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-02 00:45:03 +0800

    make path-full-info work on freebsd

--- src/clj/spire/remote.clj
+++ src/clj/spire/remote.clj
@@ -20,7 +20,7 @@
   (cond
     (#{:freebsd} os)
     (let [[_ filename hash] (re-matches #"MD5\s+\((.+)\)\s*=\s*([0-9a-fA-F]+)" line)]
-      [filename hash])
+      [hash filename])
 
     :else
     (vec (reverse (string/split line #"\s+" 2)))))
@@ -61,16 +61,36 @@
 #_
 (file-md5sums (runner) "test")
 
+(defn process-stat-mode-out [os mode-string]
+  (cond
+    (#{:freebsd} os)
+    (subs mode-string (- (count mode-string) 3))
+
+    :else
+    mode-string))
 
 (defn path-full-info [run path]
-  (let [file-info
-        (-> (run (format "find \"%s\" -type f -exec stat -c '%%a %%X %%Y %%s' {} \\; -exec md5sum {} \\;" path))
+  (let [os (facts/get-fact [:system :os])
+        md5 (facts/get-fact [:paths :md5])
+        md5sum (facts/get-fact [:paths :md5sum])
+        md5path (or md5sum md5)
+
+        file-info
+        (->
+         (cond
+           (#{:freebsd} os)
+           (run (format "find \"%s\" -type f -exec stat -f '%%p %%a %%m %%z' {} \\; -exec md5 {} \\;" path))
+
+           :else
+           (run (format "find \"%s\" -type f -exec stat -c '%%a %%X %%Y %%s' {} \\; -exec md5sum {} \\;" path))
+           )
             (string/split #"\n")
             (->> (partition 2)
                  (map (fn [[stats hashes]]
                         (let [[mode last-access last-modified size] (string/split stats #" " 4)
-                              [md5sum filename] (string/split hashes #"\s+" 2)
-                              fname (nio/relativise path filename)]
+                              [md5sum filename] (process-md5-out os hashes)
+                              fname (nio/relativise path filename)
+                              mode (process-stat-mode-out os mode)]
                           [fname {:type :file
                                   :filename fname
                                   :md5sum md5sum
@@ -83,12 +103,19 @@
                  (into {})))
 
         dir-info
-        (-> (run (format "find \"%s\" -type d -exec stat -c '%%a %%X %%Y %%s %%n' {} \\;" path))
+        (-> (cond
+              (#{:freebsd} os)
+              (run (format "find \"%s\" -type d -exec stat -f '%%p %%a %%m %%z %%N' {} \\;" path))
+
+              :else
+              (run (format "find \"%s\" -type d -exec stat -c '%%a %%X %%Y %%s %%n' {} \\;" path))
+              )
             (string/split #"\n")
             (->> (filter #(pos? (count %)))
                  (map (fn [stats]
                         (let [[mode last-access last-modified size filename] (string/split stats #" " 5)
-                              fname (nio/relativise path filename)]
+                              fname (nio/relativise path filename)
+                              mode (process-stat-mode-out os mode)]
                           [fname {:type :dir
                                   :filename fname
                                   :mode-string mode
@@ -100,3 +127,18 @@
 
 #_
 (path-full-info (runner) "test")
+
+#_ (transport/ssh
+ "root@192.168.92.237"
+ ;;"root@localhost"
+ (let [run (fn [command]
+             (let [{:keys [out exit err]}
+                   (ssh/ssh-exec spire.state/*connection* command "" "UTF-8" {})]
+               (comment
+                 (println "exit:" exit)
+                 (println "out:" out)
+                 (println "err:" err))
+               (if (zero? exit)
+                 (string/trim out)
+                 "")))]
+   (path-full-info run "/root")))
