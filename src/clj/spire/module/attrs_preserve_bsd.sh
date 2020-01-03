469904220c07fa37fa35213a9e33e3b9422fa562
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-03 15:47:03 +0800

    attrs preserve in bsd form

---
+++ src/clj/spire/module/attrs_preserve_bsd.sh
@@ -0,0 +1,35 @@
+set -e
+
+EXIT=0
+
+function set_file
+{
+  MODE="$1"
+  ACCESS="$2"
+  ACCESS_STAMP="$3"
+  MODIFIED="$4"
+  MODIFIED_STAMP="$5"
+  FILE="$6"
+
+  echo "set_file: $FILE"
+
+  FILE_TYPE_AND_MODE=$(stat -f '%p' "$FILE")
+  FILE_MODE=${FILE_TYPE_AND_MODE:$((${#FILE_TYPE_AND_MODE}-3)):3}
+  FILE_ACCESS=$(stat -f '%a' "$FILE")
+  FILE_MODIFIED=$(stat -f '%m' "$FILE")
+
+  if [ "$MODE" != "$FILE_MODE" ]; then
+    chmod "$MODE" "$FILE"
+    EXIT=-1
+  fi
+
+  if [ "$ACCESS" != "$FILE_ACCESS" ]; then
+    touch -a -d "$ACCESS_STAMP" "$FILE"
+    EXIT=-1
+  fi
+
+  if [ "$MODIFIED" != "$FILE_MODIFIED" ]; then
+    touch -m -d "$MODIFIED_STAMP" "$FILE"
+    EXIT=-1
+  fi
+}
