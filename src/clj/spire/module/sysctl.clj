e882216dcb4fd00bef893ac9095b4a1f5ec65753
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-08 16:08:47 +0800

    sysctl script

--- src/clj/spire/module/sysctl.clj
+++ src/clj/spire/module/sysctl.clj
@@ -6,12 +6,49 @@
             [spire.utils :as utils]
             [clojure.string :as string]))
 
-(utils/defmodule sysctl [file-path]
-  [host-string session]
-  (let [{:keys [exit out err] :as result}
-        (ssh/ssh-exec session
-                      (format "cat %s" (utils/path-quote file-path))
-                      ""
-                      "UTF-8" {})]
+(def failed-result {:exit 1 :out "" :err "" :result :failed})
+
+(defmulti make-script (fn [command opts] command))
+
+(defmulti preflight (fn [command opts] command))
+
+(defmulti process-result (fn [command opts result] command))
+
+;;
+;; (sysctl :present ...)
+;;
+(defmethod preflight :present [_ {:keys [name value reload file] :as opts}]
+  nil)
+
+(defmethod make-script :present [_ {:keys [name value reload file] :as opts}]
+  (utils/make-script
+   "sysctl_present.sh"
+   {}))
+
+(defmethod process-result :present
+  [_ {:keys [name value reload file] :as opts} {:keys [out err exit] :as result}]
+  (cond
+    (zero? exit)
     (assoc result
-           :result :ok)))
+           :exit 0
+           :result :ok)
+
+    (= 255 exit)
+    (assoc result
+           :exit 0
+           :result :changed)
+
+    :else
+    (assoc result
+           :result :failed)))
+
+
+(utils/defmodule sysctl [command {:keys [name value reload file] :as opts}]
+  [host-string session]
+  (or
+   (preflight command opts)
+   (->>
+    (ssh/ssh-exec session (make-script command opts) "" "UTF-8" {})
+    (process-result command opts)))
+
+  )
---
+++ src/clj/spire/module/sysctl_present.sh
@@ -0,0 +1,1 @@
+echo "sysctl_present.sh"
