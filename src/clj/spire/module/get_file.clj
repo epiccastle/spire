8124ec1984143bd7a9eeb2e5cb2cd89bcba7a040
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-08 15:24:48 +0800

    get-file

---
+++ src/clj/spire/module/get_file.clj
@@ -0,0 +1,17 @@
+(ns spire.module.get-file
+  (:require [spire.output :as output]
+            [spire.state :as state]
+            [spire.transport :as transport]
+            [spire.ssh :as ssh]
+            [spire.utils :as utils]
+            [clojure.string :as string]))
+
+(utils/defmodule get-file [file-path]
+  [host-string session]
+  (let [{:keys [exit out err] :as result}
+        (ssh/ssh-exec session
+                      (format "cat %s" (utils/path-quote file-path))
+                      ""
+                      "UTF-8" {})]
+    (assoc result
+           :result :ok)))
