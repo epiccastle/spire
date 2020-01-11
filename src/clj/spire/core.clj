d62de946b72a0a5ed0adde39883e2f42fac6acc9
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-12 01:01:43 +0800

    what is out library path?

--- src/clj/spire/core.clj
+++ src/clj/spire/core.clj
@@ -22,6 +22,8 @@
 (defn initialise []
   (config/init!)
 
+  (prn "java.library.path:" (System/getProperty "java.library.path"))
+
   ;;(System/loadLibrary "spire")
   (clojure.lang.RT/loadLibrary "spire")
   )
