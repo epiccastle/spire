d1e858ba924c38aa89e05886dd9604be8855498a
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-17 22:48:05 +0800

    bsd version of absent line

---
+++ src/clj/spire/module/line_in_file_absent_bsd.sh
@@ -0,0 +1,39 @@
+if [ ! -f "$FILE" ]; then
+  echo -n "File not found." 1>&2
+  exit 1
+fi
+
+if [ ! -r "$FILE" ]; then
+  echo -n "File not readable." 1>&2
+  exit 1
+fi
+
+SELECTOR="head -1"
+LINECOUNT=$(wc -l "$FILE" | awk '{print $1}')
+
+# :absent by linenum
+if [ "$LINENUM" ]; then
+  if [ "$LINENUM" -gt "$LINECOUNT" ]; then
+    echo -n "No line number $LINENUM in file." 1>&2
+    exit 2
+  elif [ "$LINENUM" -lt "-$LINECOUNT" ]; then
+    echo -n "No line number $LINENUM in file." 1>&2
+    exit 2
+  elif [ "$LINENUM" -lt 0 ]; then
+    LINENUM=$((LINECOUNT + LINENUM + 1))
+  fi
+
+  sed -i "" "${LINENUM}d${LINE}" "$FILE"
+  exit -1
+fi
+
+# :absent by regexp
+if [ "$REGEX" ]; then
+  LINENUM=$(sed -n "${REGEX}=" "$FILE" | $SELECTOR)
+  if [ "$LINENUM" ]; then
+    sed -i "" "${LINENUM}d${LINE}" "$FILE"
+    exit -1
+  else
+    exit 0
+  fi
+fi
