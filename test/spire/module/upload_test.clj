3e429db62d498c22f10b51c7df748cdf61bc47ba
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-17 14:48:44 +0800

    wrap formats

--- test/spire/module/upload_test.clj
+++ test/spire/module/upload_test.clj
@@ -76,21 +76,21 @@
        (with-redefs [spire.scp/scp-to no-scp]
            (is (= {:result :changed, :attr-result {:result :changed}, :copy-result {:result :ok}}
                     (upload {:src "test/files" :dest tf :recurse true :force true :preserve true})))
-           (is (= (test-utils/run "cd test/files && find . -exec %s {} \\;" (test-utils/make-stat-command ["%s" "%a" "%Y" "%X" "%F" "%n"]))
+         (is (= (test-utils/run (format "cd test/files && find . -exec %s {} \\;" (test-utils/make-stat-command ["%s" "%a" "%Y" "%X" "%F" "%n"])))
                     (test-utils/ssh-run (format "cd \"%s\" && find . -exec %s {} \\;" tf (test-utils/make-stat-command ["%s" "%a" "%Y" "%X" "%F" "%n"]))))))
 
        ;; preserve copy from scratch
        (is (= {:result :changed, :attr-result {:result :ok}, :copy-result {:result :changed}}
                 (upload {:src "test/files" :dest tf2 :recurse true :preserve true})))
-       (is (= (test-utils/run "cd test/files && find . -exec %s {} \\;" (test-utils/make-stat-command ["%s" "%a" "%Y" "%X" "%F" "%n"]))
+       (is (= (test-utils/run (format "cd test/files && find . -exec %s {} \\;" (test-utils/make-stat-command ["%s" "%a" "%Y" "%X" "%F" "%n"])))
                 (test-utils/ssh-run (format "cd \"%s\" && find . -exec %s {} \\;" tf2 (test-utils/make-stat-command ["%s" "%a" "%Y" "%X" "%F" "%n"])))))
 
        ;; mode and dir-mode from scratch
        (is (= {:result :changed, :attr-result {:result :ok}, :copy-result {:result :changed}}
                 (upload {:src "test/files" :dest tf3 :recurse true :mode 0666 :dir-mode 0777})))
-       (is (= (test-utils/run "cd test/files && find . -type f -exec %s {} \\;" (test-utils/make-stat-command ["%s" "%a" "%F" "%n"]))
+       (is (= (test-utils/run (format "cd test/files && find . -type f -exec %s {} \\;" (test-utils/make-stat-command ["%s" "%a" "%F" "%n"])))
                 (test-utils/ssh-run (format "cd \"%s\" && find . -type f -exec %s {} \\;" tf3 (test-utils/make-stat-command ["%s" "%a" "%F" "%n"])))))
-       (is (= (test-utils/run "cd test/files && find . -type d -exec %s {} \\;" (test-utils/make-stat-command ["%s" "%a" "%F" "%n"]))
+       (is (= (test-utils/run (format "cd test/files && find . -type d -exec %s {} \\;" (test-utils/make-stat-command ["%s" "%a" "%F" "%n"])))
                 (test-utils/ssh-run (format "cd \"%s\" && find . -type d -exec %s {} \\;" tf3 (test-utils/make-stat-command ["%s" "%a" "%F" "%n"])))))
 
        #_(with-redefs [spire.scp/scp-to no-scp]
