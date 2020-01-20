de945ba742d73e03fca6b5228bbb823104067e22
Crispin Wellington <retrogradeorbit@gmail.com>
2020-01-20 20:38:38 +0800

    nested ssh tests

--- test/spire/ssh_test.clj
+++ test/spire/ssh_test.clj
@@ -1,6 +1,8 @@
 (ns spire.ssh-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as string]
             [spire.transport :as transport]
+            [spire.module.shell :as shell]
             [spire.test-config :as test-config]))
 
 (deftest ssh-host-descriptions
@@ -15,3 +17,57 @@
     (transport/ssh {:strict-host-key-checking false
                     :host-string (str "root@localhost:" test-config/ssh-port)
                     :port test-config/ssh-port})))
+
+(deftest ssh-nesting
+  (testing "ssh lexical nesting tests"
+    (transport/ssh
+     test-config/localhost-root
+     (is (= "root"
+            (string/trim (:out (shell/shell {:cmd "whoami"})))))
+
+     (transport/ssh
+      test-config/localhost-root
+      (is (= "root"
+             (string/trim (:out (shell/shell {:cmd "whoami"})))))
+      )
+
+     (is (= "root"
+            (string/trim (:out (shell/shell {:cmd "whoami"})))))))
+
+  (testing "ssh lexical nesting tests"
+    (transport/ssh
+     test-config/localhost-root
+     (is (= "root"
+            (string/trim (:out (shell/shell {:cmd "whoami"})))))
+
+     (transport/ssh
+      test-config/localhost
+      (is (= (System/getProperty "user.name")
+             (string/trim (:out (shell/shell {:cmd "whoami"})))))
+
+      (transport/ssh
+       test-config/localhost-root
+       (is (= "root"
+              (string/trim (:out (shell/shell {:cmd "whoami"}))))))
+
+      (is (= (System/getProperty "user.name")
+             (string/trim (:out (shell/shell {:cmd "whoami"}))))))
+
+     (is (= "root"
+            (string/trim (:out (shell/shell {:cmd "whoami"}))))))))
+
+(deftest ssh-group-nesting
+  (testing "ssh-group lexical nesting tests"
+    (transport/ssh-group
+     [test-config/localhost-root test-config/localhost]
+     (is (#{(System/getProperty "user.name") "root"}
+          (string/trim (:out (shell/shell {:cmd "whoami"})))))
+
+
+     (transport/ssh-group
+      [test-config/localhost-root test-config/localhost]
+      (is (#{(System/getProperty "user.name") "root"}
+           (string/trim (:out (shell/shell {:cmd "whoami"}))))))
+
+     (is (#{(System/getProperty "user.name") "root"}
+          (string/trim (:out (shell/shell {:cmd "whoami"}))))))))
