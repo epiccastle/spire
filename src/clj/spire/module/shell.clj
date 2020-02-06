52f816d7d51286b70ffed9974255c3af9f546630
Crispin Wellington <retrogradeorbit@gmail.com>
2020-02-07 00:19:52 +0800

    shell module docs

--- src/clj/spire/module/shell.clj
+++ src/clj/spire/module/shell.clj
@@ -53,3 +53,55 @@
 
 (defmacro shell [& args]
   `(utils/wrap-report ~*file* ~&form (shell* ~@args)))
+
+
+(def documentation
+  {
+   :module "shell"
+   :blurb "Run commands and shell snippets on the remote hosts"
+   :description ["Run executables on the remote hosts"
+                 "Run shell scripts on the remote hosts"]
+   :form "(shell options)"
+   :args
+   [{:arg "options"
+     :desc "A hashmap of options. All available keys and their values are described below"}]
+
+   :opts
+   [
+    [:cmd
+     {:description ["The command or shell snippet to run"]
+      :type :string
+      :required true}]
+
+    [:shell
+     {:description ["Specify the shell to use to run the command. Deafult is `bash`."]
+      :type :string
+      :required false}]
+
+    [:dir
+     {:description ["Execute the command from within the specified directory."
+                    "Path can be relative or absolute."
+                    "Relative paths are specified in relation to the users home directory."
+                    "Default `:dir` is \".\" (users home directory)."]
+      :type :string
+      :required false}]
+
+    [:out
+     {:description ["Specify the encoding of the commands output."
+                    "Default is \"UTF-8\"."]
+      :type :string
+      :required false}]
+
+    [:env
+     {:description ["Specify a hashmap of environment variables to set before executing the command."
+                    "Environment variable names can be specified as keywords or strings."]
+      :type :hashmap
+      :required false}]
+
+    [:creates
+     {:description ["Specify a list of files that the command will create."
+                    "If these files exist on the remote host, the command will not be executed and the job execution will be marked as `:ok`."]
+      :type [:vector :list :lazy-seq]
+      :required false}]]})
+
+#_ [env dir shell out opts cmd creates]
