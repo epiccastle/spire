e6b398f1133cf7bd36d9c937a1e47e8df7d377e5
Crispin Wellington <retrogradeorbit@gmail.com>
2020-02-07 00:20:01 +0800

    user module docs

--- src/clj/spire/module/user.clj
+++ src/clj/spire/module/user.clj
@@ -91,3 +91,61 @@
 
 (defn gecos [{:keys [fullname room office home info]}]
   (str fullname "," room "," office "," home "," info))
+
+
+(def documentation
+  {:module "user"
+   :blurb "Manage the system users"
+   :description
+   ["This module ensures a particular user is present on, or absent from, the remote machine."
+    "It ensures that the configuration of the user matches the values specified."
+    "The user can be specified by username or by user id. If both are specified the user is found by name, and the id is set to match the id value."
+    ]
+   :form "(user command options)"
+   :args
+   [{:arg "command"
+     :desc "The state the user should be in. Should be one of `:present` or `:absent`."
+     :values
+     [[:present "Ensures the named user is present and has the specified settings"]
+      [:absent "Ensures the named user is absent"]]}
+    {:arg "options"
+     :desc "A hashmap of options. All available option keys and their values are described below"}]
+
+   :opts
+   [[:name
+     {:description ["The username"]
+      :type :string
+      :required false}]
+    [:uid
+     {:description ["The user's id number"]
+      :type :string
+      :required false}]
+    [:home
+     {:description ["The user's home directory path"]
+      :type :string
+      :required false}]
+    [:group
+     {:description ["The user's primary group."]
+      :type :string
+      :required false}]
+    [:groups
+     {:description ["A list of groups the user should belong to"]
+      :type :vector
+      :required false}]
+    [:password
+     {:description ["An encrypted password string to use for the user's password login."]
+      :type :string
+      :required false}]
+    [:shell
+     {:description ["The path to the users default shell."]
+      :type :string
+      :required false}]
+    [:comment
+     {:description ["The comment field to add against the user in the users database."]
+      :type :string
+      :required false}]]})
+
+#_
+[name comment uid home
+ group groups password shell
+ ]
