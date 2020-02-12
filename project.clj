5567e026520d9495893a894aebcc5986921efd5c
Crispin Wellington <retrogradeorbit@gmail.com>
2020-02-12 21:52:31 +0800

    project info

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject spire #= (clojure.string/trim #= (slurp ".meta/VERSION"))
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :description "Pragmatic Provisioning"
   :url "https://epiccastle.io/spire"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
