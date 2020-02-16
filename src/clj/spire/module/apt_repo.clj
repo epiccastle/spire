cafc41caf5741f4168c9d6099bd921b39bfccd02
Crispin Wellington <retrogradeorbit@gmail.com>
2020-02-17 00:26:48 +0800

    apt-repo for non ppa repos

--- src/clj/spire/module/apt_repo.clj
+++ src/clj/spire/module/apt_repo.clj
@@ -17,33 +17,54 @@
 
 (defmulti process-result (fn [command opts result] command))
 
+(defn make-filename-from-repo [repo]
+  (-> repo
+      #_ "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
+      #_ "ppa:ondrej/php"
+      (string/replace #"\[[^\]]+\]" "")
+      (string/replace #"\w+://" "")
+      (string/split #"\s+")
+      (->> (filter (complement #{"deb" "deb-src"}))
+           (string/join "___"))
+      (string/replace #"[^0-9A-Za-z]+" "_")
+      (str ".list")))
+
 ;;
 ;; (line-in-file :present ...)
 ;;
 (defmethod preflight :present [_ _]
   (facts/check-bins-present #{:sed :grep :awk :apt-key :curl}))
 
-(defmethod make-script :present [_ repo]
-    (let [ppa? (string/starts-with? repo "ppa:")]
-      (if ppa?
-        (let [[_ ppa] (string/split repo #":")
-              [ppa-owner ppa-name] (string/split ppa #"/")
-              ppa-name (or ppa-name "ppa")
-              codename (name (facts/get-fact [:system :codename]))
-              deb-line (format "deb http://ppa.launchpad.net/%s/%s/ubuntu %s main" ppa-owner ppa-name codename)
-              debsrc-line (format "deb-src http://ppa.launchpad.net/%s/%s/ubuntu %s main" ppa-owner ppa-name codename)
-              contents (str deb-line "\n" "# " debsrc-line)]
-          (utils/make-script
-           "apt_repo_present.sh"
-           {:FILE (some->
-                   "/etc/apt/sources.list.d/%s-%s-%s-%s.list"
-                   (format ppa-owner "ubuntu" ppa-name codename)
-                   utils/path-escape)
-            :CONTENTS (some->> contents utils/path-escape)
-            :PPA_NAME ppa-name
-            :PPA_OWNER ppa-owner
-            :CODENAME codename
-            })))))
+(defmethod make-script :present [_ {:keys [repo filename]}]
+  (let [ppa? (string/starts-with? repo "ppa:")
+        codename (name (facts/get-fact [:system :codename]))]
+    (if ppa?
+      ;; ppa repository source
+      (let [[_ ppa] (string/split repo #":")
+            [ppa-owner ppa-name] (string/split ppa #"/")
+            ppa-name (or ppa-name "ppa")
+            deb-line (format "deb http://ppa.launchpad.net/%s/%s/ubuntu %s main" ppa-owner ppa-name codename)
+            debsrc-line (format "deb-src http://ppa.launchpad.net/%s/%s/ubuntu %s main" ppa-owner ppa-name codename)
+            contents (str deb-line "\n" "# " debsrc-line)]
+        (utils/make-script
+         "apt_repo_present.sh"
+         {:FILE (some->
+                 "/etc/apt/sources.list.d/%s-%s-%s-%s.list"
+                 (format ppa-owner "ubuntu" ppa-name codename)
+                 utils/path-escape)
+          :CONTENTS (some->> contents utils/path-escape)
+          :PPA_NAME ppa-name
+          :PPA_OWNER ppa-owner
+          :CODENAME codename
+          }))
+
+      ;; non ppa repository source
+      (utils/make-script
+       "apt_repo_present.sh"
+       {:FILE (some-> (or filename (make-filename-from-repo repo))
+                      utils/path-escape)
+        :CONTENTS (some-> repo utils/path-escape)
+        :CODENAME codename}))))
 
 (defmethod process-result :present
   [_ _ {:keys [out err exit] :as result}]
