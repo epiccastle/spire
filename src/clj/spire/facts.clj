616e2aea02d3e3b59bd555cffc48ca941c359034
Crispin Wellington <retrogradeorbit@gmail.com>
2020-02-27 11:35:40 +0800

    delete old script packing code #9

--- src/clj/spire/facts.clj
+++ src/clj/spire/facts.clj
@@ -7,87 +7,6 @@
 
 (defonce state (atom {}))
 
-(defn ip-entry-process [[headline link & lines]]
-  (let [[f b] (string/split headline #">")
-        [num device flags] (string/split f #":\s+")
-        device (keyword device)
-        flags (-> flags
-                  string/trim
-                  (string/replace #"<" "")
-                  (string/split #",")
-                  (->> (map keyword)
-                       (into #{})))
-        opts (-> b
-                 string/trim
-                 (string/split #"\s+")
-                 (->> (partition 2)
-                      (map (fn [[k v]] [(keyword k)
-                                        (try
-                                          (Integer/parseInt v)
-                                          (catch java.lang.NumberFormatException _
-                                            (keyword v)))]))
-                      (into {}))
-                 )
-        link (-> link
-                 (string/split #"/")
-                 second
-                 (string/split #"\s+")
-                 (->> (partition 2)
-                      (map (fn [[k v]] [(keyword k)
-                                        (try
-                                          (Integer/parseInt v)
-                                          (catch java.lang.NumberFormatException _
-                                            v))]))
-                      (into {}))
-                 )
-        addresses (-> lines
-                      (->> (map string/trim)
-                           (partition 2)
-                           (map (fn [[overview flags]]
-                                  (let [result (-> overview
-                                                   (string/split #"\s+")
-                                                   (->> (partition 2)
-                                                        (map (fn [[k v]] [(keyword k)
-                                                                          (try
-                                                                            (Integer/parseInt v)
-                                                                            (catch java.lang.NumberFormatException _
-                                                                              v))]))
-                                                        (into {})))
-                                        flags (-> flags
-                                                  string/trim
-                                                  (string/split #"\s+")
-                                                  (->> (partition 2)
-                                                       (map (fn [[k v]] [(keyword k)
-                                                                         (try
-                                                                           (Integer/parseInt v)
-                                                                           (catch java.lang.NumberFormatException _
-                                                                             (keyword v)))
-                                                                         ]))
-                                                       (into {})))
-                                        result (merge result flags)]
-                                    result)))
-                           (into [])))
-        ]
-    [(keyword device) {:flags flags
-                       :options opts
-                       :link link
-                       :addresses addresses}]
-    )
-  )
-
-
-(defn ipaddress-process
-  "process 'ip address' output"
-  [out]
-  (-> out
-      (string/split #"\n")
-      (->> (partition-by #(re-find #"^\d+:" %))
-           (partition 2)
-           (map #(apply concat %))
-           (map ip-entry-process)
-           (into {})))
-  )
-
 (def bins
   [
    "bash" "ksh" "zsh" "csh" "tcsh" "fish"
@@ -120,54 +39,6 @@
     (apply str (map #(format "echo %s: `which %s`\n" % %) bins))
     ))
 
-(def char-choice "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-
-(defn rand-char []
-  (rand-nth char-choice))
-
-(def sep-length 16)
-
-(defn make-separator-slug []
-  (apply str (map (fn [_] (rand-char)) (range sep-length)))
-  )
-
-(defn make-separator
-  ([slug block-name]
-   (str "---"
-        slug
-        ":"
-        block-name
-        "---")))
-
-#_ (make-separator "paths")
-
-(defn start-block [slug block-name]
-  (format "echo \"%s\"\n" (make-separator slug block-name)))
-
-(defn make-fact-script [slug]
-  (str
-   #_ (start-block slug "paths")
-   #_ (make-which)
-   (start-block slug "shell")
-   (utils/embed-src "facts_shell.sh")
-   (start-block slug "shell-id")
-   (utils/embed-src "facts_id.sh")))
-
-(defn extract-blocks [slug {:keys [out] :as res}]
-  (let [lines (string/split-lines out)]
-    (->> lines
-         (partition-by #(string/starts-with? % (str "---" slug ":")))
-         (partition 2)
-         (map (fn [[header lines]]
-                [(let [tag (-> header
-                               first
-                               (string/split #":")
-                               second)
-                       tag (subs tag 0 (- (count tag) 3))]
-                   (keyword tag))
-                 (vec lines)]))
-         (into {}))))
-
 (defn process-uname [uname]
   (let [[kernel-name node-name kernel-release kernel-version machine processor platform os]
         (string/split uname #"\s+")]
@@ -179,10 +50,7 @@
      :platform platform
      :os os
      :node node-name
-     :string uname
-     }
-    )
-  )
+     :string uname}))
 
 (defn process-shell-uname [[command file uname platform node-name os kernel-release kernel-version]]
   {:string uname
