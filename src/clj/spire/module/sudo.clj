50692d4889e662120c2ce0a3f52552395708ee25
Crispin Wellington <retrogradeorbit@gmail.com>
2020-03-28 13:44:40 +0800

    finish PipedInputStream password prefixer proxy

--- src/clj/spire/module/sudo.clj
+++ src/clj/spire/module/sudo.clj
@@ -21,6 +21,11 @@
                       :else "")]
     (format "sudo -S -p '%s' %s %s %s" prompt user-flags group-flags command)))
 
+(defn copy-string-into-byte-array [s byte-arr off]
+  (let [len (count s)
+        char-array (byte-array (map #(int (.charAt s %)) (range len)))]
+    (System/arraycopy char-array 0 byte-arr off len)))
+
 (defn prefix-sudo-stdin [{:keys [password required?]} stdin]
   (if required?
     (cond
@@ -35,20 +40,47 @@
                (.available stdin)))
           (close [this]
             (.close stdin))
-          #_ (connect [this ^PipedOutputStream out]
-            (.connect stdin out))
           (read
             ([this]
-             (let [[old remain] (swap-vals! prefix (fn [s] (if (empty? s) s (subs s 1))))]
+             (let [[old remain] (swap-vals! prefix
+                                            (fn [s] (if (empty? s) s (subs s 1))))]
                (if (empty? old)
                  (.read stdin)
                  (int (.charAt old 0)))))
             ([this byte-arr off len]
              (if (zero? len)
                0
-               ;; TODO: transfer to buffer with prefix
-               ;; for now Im tired, just pass through
-               (.read stdin byte-arr off len))))
+               ;; transfer to buffer with prefix
+               (let [[old remain] (swap-vals! prefix
+                                              (fn [s] (if (< len (count s))
+                                                        (subs s len)
+                                                        "")))
+                     chars-to-prefix? (not (empty? old))
+                     taken-num (min len (count old))
+                     taken (subs old 0 taken-num)
+                     remain? (not (empty remain))]
+
+                 (cond
+                   (and chars-to-prefix?
+                        (= taken-num len))
+                   (do
+                     (copy-string-into-byte-array taken byte-arr off)
+                     len)
+
+                   (and chars-to-prefix?
+                        (< taken-num len))
+                   (do
+                     (copy-string-into-byte-array taken byte-arr off)
+                     (let [copied (.read stdin byte-arr (+ off taken-num) (- len taken-num))]
+                       (if (= -1 copied)
+                         taken-num
+                         (+ taken-num copied))))
+
+                   (not chars-to-prefix?)
+                   (.read stdin byte-arr off len)
+
+                   :else
+                   -1)))))
           (receive [this b]
             (.receive stdin b)))))
     stdin))
