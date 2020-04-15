8b16f337f8fc1802c9007a94ca065ace6be08ef0
Crispin Wellington <retrogradeorbit@gmail.com>
2020-04-16 00:01:42 +0800

    curl command construction

--- src/clj/spire/module/curl.clj
+++ src/clj/spire/module/curl.clj
@@ -9,18 +9,13 @@
 
 (set! *warn-on-reflection* true)
 
-(defn- accept-header [opts]
-  (when-let [accept (:accept opts)]
-    ["-H" (str "Accept: " (case accept
-                            :json "application/json"
-                            accept))]))
-
 (defn- url-encode
   "Returns an UTF-8 URL encoded version of the given string."
   [^String unencoded]
   (URLEncoder/encode unencoded "UTF-8"))
 
-(defn command [{:keys [method headers form cookies url auth query-params]
+(defn command [{:keys [method headers accept dump-header form cookies cookie-jar url auth query-params
+                       data-raw data-binary http2 output user-agent]
                 :or {method :GET}
                 :as opts}]
   (let [method-arg (case method
@@ -51,107 +46,66 @@
                              ^String (:fragment url))))
         {:keys [user password method]
          :or {method :any}} auth
+        auth-method-val (when auth
+                          (case method
+                            :any "--anyauth"
+                            :basic "--basic"
+                            :digest "--digest"
+                            :ntlm "--ntlm"
+                            :negotiate "--negotiate"))
+        user-val (when (and user password) (str user ":" password))
         query-val (some->> query-params
                            (map (fn [[k v]] (format "%s=%s" (url-encode (name k)) (url-encode (name v)))))
                            (string/join "&"))
-
         ]
-    [method-arg header-set form-set cookies-set url-val
-     user password method query-val
-     ]))
+    (->> ["curl" "-s" "-S" method-arg
+          (for [h header-set] ["-H" h])
+          (when accept ["-H" (str "Accept: " (case accept
+                                               :json "application/json"
+                                               accept))])
+          (when dump-header ["-D" dump-header])
+          (for [f form-set] ["-F" f])
+          (when cookies-set ["-b" cookies-set])
+          (when cookie-jar ["-c" cookie-jar])
+          auth-method-val
+          (when user-val ["-u" user-val])
+          (when data-raw ["--data-raw" data-raw])
+          (when data-binary ["--data-binary" data-binary])
+          (when http2 "--http2")
+          (when output ["-o" output])
+          (when user-agent ["-A" user-agent])
+          "-L" (str url-val (when query-val
+                              (str "?" query-val)))
+          ]
+         flatten
+         (filter identity)
+         (map utils/string-quote)
+         (string/join " "))))
 
-#_ (command {:method :head
-             :headers {:X-First-name "Joe"
-                       :X-Second-name "Bloggs"}
-             :form {:name "Joe Bloggs"
-                    :upload "@foo.dat"}
-             :cookies {:NAME1 "VALUE1"
-                       :NAME2 "VALUE2"}
-             :cookie-jar "my-cookie-jar-file"
-             :url {:host   "epiccastle.io"
-                   :scheme "https"
-                   :path   "/"}
-             :auth {:user "user"
-                    :password "pass"
-                    :method :basic}
-             :query-params {:foo "bar"
-                            "full name" "Joe Bloggs"
-                            }
-             })
-
-
-
-
-
-#_ (defn curl-command [opts]
-  (let [body (:body opts)
-        opts (if body
-               (cond-> opts
-                 (string? body) (assoc :data-raw body)
-                 (file? body) (assoc :in-file body))
-               opts)
-        method (when-let [method (:method opts)]
-                 (case method
-                   :head ["--head"]
-                   ["-X" (-> method name string/upper-case)]))
-        headers (:headers opts)
-        headers (loop [headers* (transient [])
-                       kvs (seq headers)]
-                  (if kvs
-                    (let [[k v] (first kvs)]
-                      (recur (reduce conj! headers* ["-H" (str k ": " v)]) (next kvs)))
-                    (persistent! headers*)))
-        accept-header (accept-header opts)
-        form-params (:form-params opts)
-        form-params (loop [params* (transient [])
-                           kvs (seq form-params)]
-                      (if kvs
-                        (let [[k v] (first kvs)]
-                          (recur (reduce conj! params* ["-F" (str k "=" v)]) (next kvs)))
-                        (persistent! params*)))
-        query-params (when-let [qp (:query-params opts)]
-                       (loop [params* (transient [])
-                              kvs (seq qp)]
-                         (if kvs
-                           (let [[k v] (first kvs)]
-                             (recur (conj! params* (str (url-encode k) "=" (url-encode v))) (next kvs)))
-                           (string/join "&" (persistent! params*)))))
-        data-raw (:data-raw opts)
-        data-raw (when data-raw
-                   ["--data-raw" data-raw])
-        url (let [url* (:url opts)]
-              (cond
-                (string? url*)
-                url*
-
-                (map? url*)
-                (str (URI. ^String (:scheme url*)
-                           ^String (:user url*)
-                           ^String (:host url*)
-                           ^Integer (:port url*)
-                           ^String (:path url*)
-                           ^String (:query url*)
-                           ^String (:fragment url*)))))
-        in-file (:in-file opts)
-        in-file (when in-file ["-d" (str "@" (.getCanonicalPath ^File in-file))])
-        basic-auth (:basic-auth opts)
-        basic-auth (if (sequential? basic-auth)
-                     (string/join ":" basic-auth)
-                     basic-auth)
-        basic-auth (when basic-auth
-                     ["--user" basic-auth])
-        ;;header-file (.getPath ^File (:header-file opts))
-        stream? (identical? :stream (:as opts))]
-    (conj (reduce into ["curl" "--silent" "--show-error" "--location" "--dump-header" ;;header-file
-                        ]
-                  [method headers accept-header data-raw in-file basic-auth
-                   form-params
-                   ;; tested with SSE server, e.g. https://github.com/enkot/SSE-Fake-Server
-                   (when stream? ["-N"])
-                   (:raw-args opts)])
-          (str url
-               (when query-params
-                 (str "?" query-params))))))
+#_ (-> (command {:method :get
+                 :headers {:X-First-name "Joe"
+                           :X-Second-name "Bloggs"}
+                 ;; :form {:name "Joe Bloggs"
+                 ;;        :upload "@foo.dat"}
+                 :cookies {:NAME1 "VALUE1"
+                           :NAME2 "VALUE2"}
+                 :cookie-jar "my-cookie-jar-file"
+                 :url {:host   "epiccastle.io"
+                       :scheme "https"
+                       :path   "/"}
+                 :auth {:user "user"
+                        :password "pass"
+                        :method :basic}
+                 :query-params {:foo "bar"
+                                "full name" "Joe Bloggs"
+                                }
+                 :data-raw "foo"
+                 :data-binary "bar"
+                 :http2 true
+                 :output "filename.dat"
+                 :user-agent "Chrome/1.1"
+                 })
+       println)
 
 ;;;; End utils
 
