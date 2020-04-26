9f0737e6d5ee51e9346b52d2b19c97f1d9c073fd
Crispin Wellington <retrogradeorbit@gmail.com>
2020-04-26 20:52:17 +0800

    sci binding building macro

---
+++ src/clj/spire/sci.clj
@@ -0,0 +1,20 @@
+(ns spire.sci)
+
+;; some helper macros for building sci bindings
+
+(defmacro make-sci-bindings [namespace & [{:keys [exclusions]
+                                           :or {exclusions #{}}}]]
+  (->> (ns-publics namespace)
+       (map second)
+       (map (juxt identity meta))
+       (map
+        (fn [[var* {:keys [ns name macro]}]]
+          (when (not (exclusions name))
+            [(list 'quote name)
+             (if macro
+               `(with-meta (deref (var ~(symbol var*))) {:sci/macro true})
+               (symbol var*))])))
+       (filter identity)
+       (into {})))
+
+#_ (macroexpand-1 '(make-sci-bindings spire.utils))
