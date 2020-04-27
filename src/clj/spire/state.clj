55b82d12c701f3fe8c6dda73123ff40ac91d1c1d
Crispin Wellington <retrogradeorbit@gmail.com>
2020-04-27 23:23:01 +0800

    default execution context is local

--- src/clj/spire/state.clj
+++ src/clj/spire/state.clj
@@ -1,5 +1,6 @@
 (ns spire.state
-  (:require [sci.core :as sci]))
+  (:require [sci.core :as sci]
+            [spire.local :as local]))
 
 ;; all the open ssh connections
 ;; keys => host-string
@@ -8,13 +9,18 @@
   (atom {}))
 
 ;; the host config hashmap for the present executing context
-(def host-config (sci/new-dynamic-var 'host-config nil))
+(def host-config (sci/new-dynamic-var 'host-config {:key "local"}))
 
 ;; the ssh session
 (def connection (sci/new-dynamic-var 'connection nil))
 
 ;; the execution context. Used for priviledge escalation currently
-(def shell-context (sci/new-dynamic-var 'shell-context nil))
+(def shell-context (sci/new-dynamic-var
+                    'shell-context
+                    {:exec :local
+                     :exec-fn local/local-exec
+                     :shell-fn identity
+                     :stdin-fn identity}))
 
 ;; the output module
 (def output-module (sci/new-dynamic-var 'output-module nil))
