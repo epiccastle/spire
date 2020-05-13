f0b7640534bed5c879ee226f5d202812d23b12b4
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-14 00:10:20 +0800

    build the sci namespace once and refer to it in bindings

--- src/clj/spire/sci.clj
+++ src/clj/spire/sci.clj
@@ -7,32 +7,32 @@
 (defmacro make-sci-bindings [namespace & [{:keys [exclusions only]
                                            :or {exclusions #{}
                                                 only (constantly true)}}]]
-  (->> (ns-publics namespace)
-       (map second)
-       (map (juxt identity meta))
-       (map
-        (fn [[var* {:keys [ns name macro]}]]
-          (when (and
-                 (only name)
-                 (not (exclusions name)))
-            [(list 'quote name)
-             (if macro
-               `(with-meta (deref (var ~(symbol var*)))
-                  (-> (meta (var ~(symbol var*)))
-                                 (select-keys [:doc :name :arglists])
-                                 (assoc :sci.impl/built-in true
-                                        :sci/macro true
-                                        :ns (vars/->SciNamespace
-                                             (quote ~ns)
-                                             nil))))
-               `(sci/new-var (quote ~name) ~(symbol var*)
-                             (-> (meta (var ~(symbol var*)))
-                                 (select-keys [:doc :name :arglists])
-                                 (assoc :sci.impl/built-in true
-                                        :ns (vars/->SciNamespace
-                                             (quote ~ns)
-                                             nil)))))])))
-       (filter identity)
-       (into {})))
+  (let [ns-sym (gensym "ns-")
+        mapping
+        (->> (ns-publics namespace)
+             (map second)
+             (map (juxt identity meta))
+             (map
+              (fn [[var* {:keys [ns name macro]}]]
+                (when (and
+                       (only name)
+                       (not (exclusions name)))
+                  [(list 'quote name)
+                   (if macro
+                     `(with-meta (deref (var ~(symbol var*)))
+                        (-> (meta (var ~(symbol var*)))
+                            (select-keys [:doc :name :arglists])
+                            (assoc :sci.impl/built-in true
+                                   :sci/macro true
+                                   :ns ~ns-sym)))
+                     `(sci/new-var (quote ~name) ~(symbol var*)
+                                   (-> (meta (var ~(symbol var*)))
+                                       (select-keys [:doc :name :arglists])
+                                       (assoc :sci.impl/built-in true
+                                              :ns ~ns-sym))))])))
+             (filter identity)
+             (into {}))]
+    `(let [~ns-sym (vars/->SciNamespace (quote ~namespace) nil)]
+       ~mapping)))
 
 #_ (macroexpand-1 '(make-sci-bindings spire.utils))
