ce98a0334f1703d62ce7d084a53b7ea0fd8d227b
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-14 17:03:30 +0800

    lots of docstrings and a terminal width call wrapper

--- src/clj/spire/utils.clj
+++ src/clj/spire/utils.clj
@@ -131,8 +131,22 @@
     :else
     (format "%d B/s" (int bps))))
 
-(defn eta-string [sec]
-  (let [s (-> sec (rem 60))
+(defn eta-string
+  "given an numerical input in seconds, returs a human readable string
+  form.
+
+  (eta-string 21.3)
+  ;;=> \"21s\"
+  (eta-string 2967)
+  ;;=> \"49m27s\"
+  (eta-string 5967)
+  ;;=> \"1h39m27s\"
+  (eta-string 99999)
+  ;;=> \"27h46m39s\"
+  "
+  [sec]
+  (let [sec (int sec)
+        s (-> sec (rem 60))
         m (-> sec (quot 60) (rem 60))
         h (-> sec (quot 60) (quot 60))]
     (cond
@@ -145,7 +159,14 @@
       :else
       (format "%dh%02dm%02ds" h m s))))
 
-(defn executing-bin-path []
+(defn executing-bin-path
+  "On a unix system with a /proc filesystem (Linux, FreeBSD with procfs) return
+  the full path of the binary that has launched this process
+
+  (executing-bin-path)
+  ;;=> \"/usr/lib/jvm/java-8-oracle/jre/bin/java\"
+  "
+  []
   (.getCanonicalPath (io/as-file "/proc/self/exe")))
 
 (defmulti content-size type)
@@ -173,9 +194,43 @@
 (defmethod content-stream java.lang.String [f] (io/input-stream (.getBytes ^String f)))
 (defmethod content-stream (Class/forName "[B") [f] (io/input-stream f))
 
-(defn progress-bar [bytes total frac {:keys [start-time start-bytes]}]
+(defn get-terminal-width
+  "Returns a clean version of the terminal width. If the JVM is running in a
+  completely isolated JVM, the C library cannot get the terminal with (the ioctl
+  fails) and the width is reported as 61542 (on linux). If you run under a trampoline
+  (with `lein trampoline`) or as a graal native binary, the terminal width is reported
+  correctly. This `get-terminal-width` call sanitises this return value so it can
+  be used without problems in a REPL (so as to not print 61000 character long lines).
+  In these cases, the width is reported as 80.
+  "
+  []
+  (let [width (SpireUtils/get_terminal_width)]
+    (if (< 10000 width) 80 width)))
+
+(defn progress-bar
+  "given the arguments `bytes`, `total`, `frac` and `state`, print to `*out*` a
+  status bar as a side-effect. Then return the new `state` (to be passed back into
+  progress-bar later)
+
+  (progress-bar 45 2984 0.2 nil)
+  ;;stdout: |                                                | 20%
+  ;;=> {:start-time #clj-time/date-time \"2020-05-14T04:55:20.197Z\",
+  ;;    :start-bytes 45}
+
+  Calling with a passed in state adds data transfer rate and eta:
+
+  (progress-bar 45 2984 0.2
+    {:start-time (clj-time.core/minus
+                   (clj-time.core/now)
+                   (clj-time.core/seconds 5))
+     :start-bytes 20})
+  ;;stdout: |                                                | 20% 5 B/s eta:9m47s
+  ;;=> {:start-time #clj-time/date-time \"2020-05-14T05:09:33.210Z\",
+        :start-bytes 20}
+  "
+  [bytes total frac {:keys [start-time start-bytes]}]
   (let [
-        columns (SpireUtils/get_terminal_width)
+        columns (get-terminal-width)
         now (time/now)
         first? (not start-time)
 
@@ -208,11 +263,41 @@
     {:start-time (or start-time now)
      :start-bytes (or start-bytes bytes)}))
 
-(defn progress-stats [file bytes total frac
-                      fileset-total max-filename-length
-                      {:keys [start-time start-bytes fileset-file-start]}]
+(defn progress-stats
+  "given:
+
+  `file`: The object currently being copied. A java.io.File, a string
+  of content, a byte array, or something else perhaps.
+
+  `bytes`: How many bytes of this file have been transfered so far.
+
+  `total`: the total number of bytes in this file.
+
+  `frac`: the progress bar amount represented as a fraction. TODO: do
+  we need to even pass this in? cant it be calculated?
+
+  `fileset-total`: If the file being copied is part of a set of files,
+  then how many total bytes does the entire set occupy
+
+  `max-filename-length`: Of all the files in the fileset being copied,
+  what is the length of the longest filename. Used for space padding
+  the filenames to be consistent
+
+  `context`: the copy progress state (to be returned and passed back)
+
+  returns:
+
+  A hashmap of two parts:
+
+  `:progress` what gets passed to `progress-bar-from-stats`
+
+  `:context` what gets passed back into the next `progress-stats` call
+  "
+  [file bytes total frac
+   fileset-total max-filename-length
+   {:keys [start-time start-bytes fileset-file-start]}]
   (let [
-        columns (SpireUtils/get_terminal_width)
+        columns (get-terminal-width)
         now (time/now)
         first? (not start-time)
         fileset-file-start (or fileset-file-start 0)
@@ -247,8 +332,8 @@
      ;; what gets passed to the progress bar output renderer
      :progress {:file file
                 :max-filename-length max-filename-length
-                :bytes fileset-copied-so-far ;;bytes
-                :total fileset-total ;;total
+                :bytes fileset-copied-so-far                  ;;bytes
+                :total fileset-total                          ;;total
                 :frac (/ fileset-copied-so-far fileset-total) ;; frac
                 :bytes-per-second bytes-per-second
                 :eta eta}
@@ -402,10 +487,23 @@
 #_ (content-file? (io/file "./spire"))
 #_ (macroexpand-1 '(on-os :freebsd (do 1 2) #{:linux} (do 3 4)))
 
-(defn changed? [{:keys [result]}]
+(defn changed?
+  "when passed the result from a module, return true is the module had to
+  change something.
+
+  (changed? {:exit 0 :out \"file written\" :err \"\" :result :changed})
+  ;;=> true
+
+  (changed? {:exit 0 :out \"\" :err \"error\" :result :ok})
+  ;;=> false
+  "
+  [{:keys [result]}]
   (= :changed result))
 
-(defmacro failed? [& body]
+(defmacro failed?
+  "when wrapped around the body, captures a result or failure exception.
+  returns `true` if the body failed. Returns `false` if the body succeeded."
+  [& body]
   `(try
      (do
        ~@body
