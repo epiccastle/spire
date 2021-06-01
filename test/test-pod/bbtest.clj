ad01abb4231f4b119c228e25dac22601d342b058
Crispin Wellington <retrogradeorbit@gmail.com>
2021-06-01 22:17:18 +0800

    our pod test script

---
+++ test/test-pod/bbtest.clj
@@ -0,0 +1,33 @@
+(ns testbootlegpod
+  (:require [babashka.pods :as pods]))
+
+(if *command-line-args*
+  (pods/load-pod *command-line-args* {:transport :socket})
+  (pods/load-pod ["lein" "trampoline" "run"] {:transport :socket}))
+
+(require '[pod.epiccastle.spire.transport :as transport]
+         '[pod.epiccastle.spire.ssh :as ssh])
+
+(comment
+  (prn ssh/ctrl-c)
+  (prn ssh/utf-8)
+  (prn ssh/*piped-stream-buffer-size*)
+  (println ssh/debug)
+
+  (prn (ssh/make-user-info {}))
+
+  (prn (ssh/raw-mode-read-line))
+
+  (prn (ssh/make-user-info {}))
+
+  (prn (ssh/print-flush-ask-yes-no "are you sure?")))
+
+(let [session (transport/connect {:username "crispin" :hostname "localhost" :port 22})]
+  (prn session)
+  (transport/disconnect session))
+
+(let [session (transport/open-connection {:username "crispin" :hostname "localhost" :port 22})]
+  (prn session)
+  (transport/close-connection {:username "crispin" :hostname "localhost" :port 22}))
+
+#_(transport/ssh "crispin@localhost")
