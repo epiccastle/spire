6de68212ed8ea532107914d4bdf697caefdae372
Crispin Wellington <retrogradeorbit@gmail.com>
2021-09-07 12:37:48 +0800

    more spire.ssh mapping and translations

--- src/clj/spire/pod/lookup.clj
+++ src/clj/spire/pod/lookup.clj
@@ -9,6 +9,7 @@
 
 (def user-info-state (mapping/make-mapping))
 (def session-state (mapping/make-mapping))
+(def agent-state (mapping/make-mapping))
 
 (defmacro make-plain-lookup [ns-to syms]
   (let [ns-from (str "pod.epiccastle." (str ns-to))]
@@ -29,66 +30,73 @@
    {'pod.epiccastle.spire.ssh/make-user-info
     (fn [& args]
       (mapping/add-instance!
-       user-info-state (apply spire.ssh/make-user-info args)
+       user-info-state
+       (apply spire.ssh/make-user-info args)
        "pod.epiccastle.spire.ssh" "user-info"))
 
-    'pod.epiccastle.spire.ssh/raw-mode-read-line
-    spire.ssh/raw-mode-read-line
+    'pod.epiccastle.spire.ssh/make-session
+    (fn [agent-key & args]
+      (mapping/add-instance!
+       session-state
+       (apply spire.ssh/make-session (mapping/get-instance-for-key agent-state agent-key) args)
+       "pod.epiccastle.spire.ssh" "session"))
 
-    'pod.epiccastle.spire.ssh/print-flush-ask-yes-no
-    spire.ssh/print-flush-ask-yes-no
-
-    'pod.epiccastle.spire.ssh/host-description-to-host-config
-    spire.ssh/host-description-to-host-config
+    'pod.epiccastle.spire.ssh/ssh-exec-proc
+    (fn [session-key & args]
+      (apply spire.ssh/ssh-exec-proc
+             (mapping/get-instance-for-key session-state session-key)
+             args))
 
     'pod.epiccastle.spire.ssh/ssh-exec
     (fn [session-key & args]
       (apply spire.ssh/ssh-exec
-       (mapping/get-instance-for-key session-state session-key)
-       args))
+             (mapping/get-instance-for-key session-state session-key)
+             args))
+     }
+
+   (into
+    (make-plain-lookup
+     "spire.ssh"
+     [raw-mode-read-line
+      print-flush-ask-yes-no
+      host-description-to-host-config
+      ]))
 
 
-    ;;'pod.epiccastle.spire.ssh/make-session
-    ;;spire.ssh/make-session
-
-    ;; 'pod.epiccastle.spire.ssh/ssh-exec-proc
-    ;; spire.ssh/ssh-exec-proc
 
 
 
+   (into
+    {'pod.epiccastle.spire.transport/connect
+     (fn [& args]
+       (let [result (apply spire.transport/connect args)]
+         (mapping/add-instance!
+          session-state result
+          "pod.epiccastle.spire.transport" "session")))
 
-    'pod.epiccastle.spire.transport/connect
-    (fn [& args]
-      (let [result (apply spire.transport/connect args)]
-        (mapping/add-instance!
-         session-state result
-         "pod.epiccastle.spire.transport" "session")))
+     'pod.epiccastle.spire.transport/disconnect
+     (fn [& args]
+       (let [connection (mapping/get-instance-for-key session-state (first args))]
+         (spire.transport/disconnect connection)))
 
-    'pod.epiccastle.spire.transport/disconnect
-    (fn [& args]
-      (let [connection (mapping/get-instance-for-key session-state (first args))]
-        (spire.transport/disconnect connection)))
+     'pod.epiccastle.spire.transport/disconnect-all!
+     spire.transport/disconnect-all!
 
-    'pod.epiccastle.spire.transport/disconnect-all!
-    spire.transport/disconnect-all!
+     'pod.epiccastle.spire.transport/open-connection
+     (fn [& args]
+       (mapping/add-instance!
+        session-state (apply spire.transport/open-connection args)
+        "pod.epiccastle.spire.transport" "session"))
 
-    'pod.epiccastle.spire.transport/open-connection
-    (fn [& args]
-      (mapping/add-instance!
-       session-state (apply spire.transport/open-connection args)
-       "pod.epiccastle.spire.transport" "session"))
+     'pod.epiccastle.spire.transport/close-connection
+     spire.transport/close-connection
 
-    'pod.epiccastle.spire.transport/close-connection
-    spire.transport/close-connection
+     'pod.epiccastle.spire.transport/get-connection
+     (fn [& args]
+       (mapping/get-key-for-instance session-state (apply spire.transport/get-connection args)))
 
-    'pod.epiccastle.spire.transport/get-connection
-    (fn [& args]
-      (mapping/get-key-for-instance session-state (apply spire.transport/get-connection args)))
-
-    'pod.epiccastle.spire.transport/flush-out
-    spire.transport/flush-out
-
-    }
+     'pod.epiccastle.spire.transport/flush-out
+     spire.transport/flush-out})
 
    (into
     (make-plain-lookup
