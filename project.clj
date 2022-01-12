575abac75269af253b5796f6b7d841739a369012
Crispin Wellington <retrogradeorbit@gmail.com>
2022-01-12 23:38:42 +0800

    bump clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://epiccastle.io/spire"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "0.6.532"]
                  [org.clojure/tools.cli "0.4.2"]
                  [borkdude/sci "0.1.1-alpha.1"]
