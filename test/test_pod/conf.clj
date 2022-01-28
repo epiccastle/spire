1e2be8eb25a52377964e65e67074f34010a028a7
Crispin Wellington <retrogradeorbit@gmail.com>
2022-01-28 17:01:53 +0800

    sudo password into conf

--- test/test_pod/conf.clj
+++ test/test_pod/conf.clj
@@ -2,3 +2,4 @@
 
 (def username (or (System/getenv "TEST_USER") (System/getenv "USER")))
 (def hostname (or (System/getenv "TEST_HOST") "localhost"))
+(def sudo-password (System/getenv "TEST_SUDO_PASSWORD"))
