afd090a4a3df9fbae3d741be71d3d24437ffc4f9
Crispin Wellington <retrogradeorbit@gmail.com>
2022-01-28 16:52:19 +0800

    print streams for :quiet output handler

--- src/clj/spire/output/quiet.clj
+++ src/clj/spire/output/quiet.clj
@@ -12,3 +12,9 @@
 (defmethod output/debug-result :quiet [driver file form file-meta host-config result])
 
 (defmethod output/print-progress :quiet [driver file form form-meta host-string {:keys [progress context]}])
+
+(defmethod output/print-streams :quiet [driver file form form-meta host-string stdout stderr]
+  (prn 'stdout stdout 'stderr stderr)
+  (when stdout (print stdout) (.flush ^java.io.OutputStreamWriter *out*))
+  (when stderr (binding [*out* *err*] (print stderr) (.flush ^java.io.PrintWriter *err*)))
+  )
