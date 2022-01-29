216cce015b785be1e01d66b4fcfc8114d88e8db2
Crispin Wellington <retrogradeorbit@gmail.com>
2022-01-29 22:41:52 +0800

    username with newline

--- test/test_pod/conf.clj
+++ test/test_pod/conf.clj
@@ -1,5 +1,6 @@
 (ns test-pod.conf)
 
 (def username (or (System/getenv "TEST_USER") (System/getenv "USER")))
+(def username-n (str username "\n"))
 (def hostname (or (System/getenv "TEST_HOST") "localhost"))
 (def sudo-password (System/getenv "TEST_SUDO_PASSWORD"))
