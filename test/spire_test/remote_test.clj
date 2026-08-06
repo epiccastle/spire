5bae39d071d22257301b0e798b6bd900e1510530
Crispin Wellington <retrogradeorbit@gmail.com>
2026-08-06 23:30:09 +0800

    upload test

--- test/spire_test/remote_test.clj
+++ test/spire_test/remote_test.clj
@@ -3,6 +3,8 @@
             [clojure.spec.alpha :as s]
             [spire.remote :refer :all]
             [spire.transport :as transport]
+            [spire.module.upload :as upload]
+            [spire.module.download :as download]
             [spire.facts :as facts]
             [spire.state :as state]
             [clojure.pprint :as pprint]
@@ -21,4 +23,21 @@
                        (let [facts (@facts/state (str (or username "root") "@localhost:" port))]
                          (is (s/valid? ::facts-spec/system facts)
                              (s/explain-str ::facts-spec/system facts)))))))
+  )
+
+(deftest ssh-2
+  (doseq [host (config/select-hosts {:only #{:ubuntu}})]
+    (let [{:keys [port username]} (config/host-ports host)]
+      (testing (str "upload and download for " host)
+        (transport/ssh
+          {:username (or username "root")
+           :port port
+           :hostname "localhost"
+           :password "root-access-please"
+           :strict-host-key-checking false}
+          (prn (upload/upload {:content "sample data"
+                               :dest "/tmp/sample.txt"}))
+
+          ))))
+
   )
